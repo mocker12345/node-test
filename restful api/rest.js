e796888fee1a7057664cc737eeba1264e2d7ec42
rancongjie <95633676@qq.com>
2015-12-06 17:15:41 +0800

    rest

--- restful api/rest.js
+++ restful api/rest.js
@@ -4,14 +4,41 @@
 var express = require('express');
 var fs = require('fs');
 var api = express();
-api.get('/listuser', function (req,res) {
-  fs.readFile(__dirname+'/user.json','utf-8',function(err,data){
-    if (err){
+var bodyParser = require('body-parser');
+var urlen = bodyParser.urlencoded({extended: false});
+api.get('/listuser', function (req, res) {
+  fs.readFile(__dirname + '/user.json', 'utf-8', function (err, data) {
+    if (err) {
       return console.log(err);
-    }else {
+    } else {
       res.end(data);
     }
   });
+});
+api.post('/adduser', urlen, function (req, res) {
+  //res.set({'Content-Type': 'text/json', 'Encodeing': 'utf-8'});
+  fs.readFile(__dirname + '/user.json', 'utf-8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    } else {
+      console.log(req.body.name);
+      data["user3"] = req.body;
+      res.send('添加成功');
+    }
+  })
+});
+
+api.get('/search/:id/', function (req,res) {
+  fs.readFile(__dirname+'/user.json','utf-8', function (err,data) {
+    if (err){
+      return console.log(err);
+    }else {
+      console.log(data);
+      var data=JSON.parse(data);
+      var user = data['user'+req.params.id];
+      res.send(user);
+    }
+  })
 });
 
 
